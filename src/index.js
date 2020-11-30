d782565490d1c376738d12cd684fe5c038d48e0f
kurwitz3 <kurwitz3@aol.com>
2020-11-30 17:33:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+ fetch("http://localhost:3000/toys")
+ .then(resp => resp.json())
+ .then(data => {findToys(data)})
+
+ function findToys(toys){
+   toys.forEach(toy =>{
+    const div = document.createElement('div')
+    const container = document.getElementById('toy-collection')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const p = document.createElement('p')
+    const butt = document.createElement('button')
+      div.className = 'card'
+      h2.innerText = toy.name
+      img.src = toy.image
+      img.className = "toy-avatar"
+      butt.className = "like-btn"
+      butt.innerText = "Like <3"
+      p.innerText =  toy.likes 
+      div.innerHTML += h2.outerHTML + img.outerHTML + p.outerHTML + butt.outerHTML
+      container.append(div)
+   });
+ }
+  butt.addEventListener('click',function(){
+    
+  })
+
+
