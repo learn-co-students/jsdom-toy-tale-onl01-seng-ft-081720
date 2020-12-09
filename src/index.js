fc1ebc277a8fa7740cfa8ba67fe546c3b1608e1b
Bri8102 <damioniru16@gmail.com>
2020-12-08 19:36:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 let addToy = false;
 
+function fetchToys(){
+  return fetch('http://localhost:3000/toys')
+     .then(function(response) {
+       return response.json()
+     })
+     .then(function(object){
+       renderToys(object)
+     })
+}
+
+function renderToys(toys) {
+  const toyWrapper = document.getElementById('toy-collection')
+
+  for(let i = 0; i < toys.length; i++) {
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const likes = document.createElement('p')
+    const likeBtn = document.createElement('button')
+
+    h2.innerText = toys[i].name
+    img.src = toys[i].image
+    img.width = "150"
+    likes.innerText = `${toys[i].likes} likes`
+    likeBtn.innerText = "Like"
+
+    toyWrapper.appendChild(h2)
+    toyWrapper.appendChild(img)
+    toyWrapper.appendChild(likes)
+    toyWrapper.appendChild(likeBtn)
+
+    console.log(toys[i].name)
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
